83d7ad60d58062c4172b5f0d951335a8c8e297b2
tandukion <tandukion@hotmail.com>
2018-01-26 17:42:19 +0900

    pot5_zero log

--- main.cpp
+++ main.cpp
@@ -140,7 +140,7 @@ int muscle_ch [NUM_OF_MUSCLE] = {IL_R,GMAX_R,VAS_R,HAM_R,TA_R,SOL_R,ADD_R,
 																 IL_L,GMAX_L,VAS_L,HAM_L,TA_L,SOL_L,ADD_L};
 
 /* Potentiometer reference for zero degree */
-int Pot_straight [10] = {2128,2298,2305,3120,2336,2130,1446,1948,2118,2028};
+int Pot_straight [10] = {2128,2298,2305,3120,2336,2130,1446,1948,2118,2010};
 
 /* Variable for IMU Data */
 struct IMUDataArray{
@@ -925,8 +925,8 @@ int main(int argc, char *argv[]) {
 			laps1 = clock();
 
 
-			setState(IL_L,0.5);
-			//setState(1,0.7);
+			setState(IL_L,0.2);
+			setState(GMAX_L,0.5);
 
 	    for (i=0;i<SampleNum;i++){
 	      read_sensor_all(i,SensorData,JointAngle);
